47bf964e913c28f81fd10167bddb44a9ebc0057e
malccosh <malccosh@amazon.com>
2018-09-16 03:53:43 -0600

    Actually added SingletonBase abstract class

---
+++ Assets/Scripts/Utils/SingletonBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public abstract class SingletonBase<T> : MonoBehaviour {
+
+    private static T instance;
+    public static T Instance {
+        get {
+            if (!instance) {
+                instance = FindObjectOfType(typeof(T)) as T;
+                if (!instance) {
+                    Debug.LogError($"There needs to be one active {typeof(T)} script on a GameObject in your scene.");
+                }
+            }
+            return instance;
+        }
+    }
+}
